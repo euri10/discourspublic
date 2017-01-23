7b894ebe28b0b2309da9e394f389efefba0bc930
euri10 <benoit.barthelet@gmail.com>
2017-01-23 15:45:10 +0100

    scope2 performs better, the redraw is the main cost

--- mldiscours/plot.py
+++ mldiscours/plot.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import matplotlib
+import time
+
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 from keras.models import Sequential
@@ -10,7 +12,7 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.lines import Line2D
 
 np.random.seed(0)
-x = np.linspace(0, 2 * math.pi, 200)
+x = np.linspace(0, 2 * math.pi, 100000)
 sine = np.sin(x)
 err = np.random.normal(0, 0.2, len(sine))
 y = sine + err
@@ -30,26 +32,108 @@ class Scope(object):
         self.y = [0]
         self.line = Line2D(self.x, self.y)
         self.ax.add_line(self.line)
-        # self.ax.set_ylim(-.1, 1.1)
-        # self.ax.set_xlim(0, 10000)
         self.ax.set_ylim(-.1, 1.1)
         self.ax.set_xlim(0, len(x))
         plt.ion()
 
-    def tester(self, seen, x=x, y=y):
+    def update(self, seen, x=x, y=y):
         self.ax.set_xlim(0, len(x))
         self.line.set_data(x, y)
-        self.ax.figure.canvas.draw()
-
+        self._redraw()
         return self.line,
 
+    def _redraw(self):
+        self.ax.figure.canvas.draw()
+
+class Scope2(object):
+    def __init__(self, ax, verbose=1, interval=5):
+        '''
+            @param target: total number of steps expected
+            @param interval: minimum visual progress update interval (in seconds)
+        '''
+        self.ax = ax
+        self.x = [0]
+        self.y = [0]
+        self.line = Line2D(self.x, self.y)
+        self.ax.add_line(self.line)
+        self.ax.set_ylim(-.1, 1.1)
+        self.ax.set_xlim(0, len(x))
+        plt.ion()
+
+        self.last_update = 0
+        self.interval = interval
+        self.verbose = verbose
+
+    def update(self, seen, x=x, y=y):
+        '''
+            @param current: index of current step
+            @param values: list of tuples (name, value_for_last_step).
+            The progress bar will display averages for these values.
+            @param force: force visual progress update
+        '''
+
+        print(self.last_update)
+        now = time.time()
+        if self.verbose == 1:
+            if (now - self.last_update) < self.interval:
+
+                return
+            else:
+                # print('NONONO')
+                self.ax.set_xlim(0, len(x))
+                self.line.set_data(x, y)
+                self.ax.figure.canvas.draw()
+                self.last_update = now
+                return self.line,
+
+
+
+
+class DummyCallback(Callback):
+    def on_train_begin(self, logs={}):
+        self.verbose = self.params['verbose']
+        self.nb_epoch = self.params['nb_epoch']
+        self.nb_sample = self.params['nb_sample']
+        self.batch_size = self.params['batch_size']
+        self.i = 0
+        self.x = np.zeros((int((self.nb_sample / self.batch_size)) + 1) * self.nb_epoch)
+        self.y = np.zeros((int((self.nb_sample / self.batch_size)) + 1) * self.nb_epoch)
+
+
+    def on_epoch_begin(self, epoch, logs={}):
+        if self.verbose:
+            print('Epoch %d/%d' % (epoch + 1, self.nb_epoch))
+        self.seen = 0
+
+    def on_batch_begin(self, batch, logs={}):
+        if self.seen < self.params['nb_sample']:
+            self.log_values = []
+
+    def on_batch_end(self, batch, logs={}):
+        batch_size = logs.get('size', 0)
+        self.seen += batch_size
+        print('\n {}'.format(self.seen))
+        self.x[self.i] = self.i
+        self.y[self.i] = logs['loss']
+        self.i += 1
+        for k in self.params['metrics']:
+            if k in logs:
+                self.log_values.append((k, logs[k]))
+
+
+    def on_epoch_end(self, epoch, logs={}):
+        for k in self.params['metrics']:
+            if k in logs:
+                self.log_values.append((k, logs[k]))
+
+    def on_train_end(self, logs={}):
+        print('training done')
 
 class TrainingHistory(Callback):
     """Callback that outputs a live plot.
     """
     def __init__(self, scope):
         self.scope = scope
-
 
     def on_train_begin(self, logs={}):
         self.verbose = self.params['verbose']
@@ -73,6 +157,7 @@ class TrainingHistory(Callback):
     def on_batch_end(self, batch, logs={}):
         batch_size = logs.get('size', 0)
         self.seen += batch_size
+        # print('\n {}'.format(self.seen))
         self.x[self.i] = self.i
         self.y[self.i] = logs['loss']
         self.i += 1
@@ -83,7 +168,7 @@ class TrainingHistory(Callback):
         # skip progbar update for the last batch;
         # will be handled by on_epoch_end
         if self.verbose and self.seen < self.params['nb_sample']:
-            self.scope.tester(self.seen, self.x[:self.i], self.y[:self.i])
+            self.scope.update(self.seen, self.x[:self.i], self.y[:self.i])
 
     def on_epoch_end(self, epoch, logs={}):
         for k in self.params['metrics']:
@@ -91,7 +176,7 @@ class TrainingHistory(Callback):
                 self.log_values.append((k, logs[k]))
         if self.verbose:
             # self.scope.update(self.seen)
-            self.scope.tester(self.seen, self.x[:self.i], self.y[:self.i])
+            self.scope.update(self.seen, self.x[:self.i], self.y[:self.i])
 
     def on_train_end(self, logs={}):
         print('training done')
@@ -99,20 +184,21 @@ class TrainingHistory(Callback):
 X_train = np.array(x, ndmin=2).T
 Y_train = np.array(y, ndmin=2).T
 
-
+t0 = time.time()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-scope = Scope(ax)
-ani = FuncAnimation(fig, scope.tester, interval=10, blit=False)
+scope = Scope2(ax)
+ani = FuncAnimation(fig, scope.update)
 #ion needed in scope
 plt.show()
 history = TrainingHistory(scope=scope)
-res = model.fit(X_train,
-          Y_train,
-          nb_epoch=10,
-          verbose=1,
-          callbacks=[history])
+dummy = DummyCallback()
+# res = model.fit(X_train, Y_train, nb_epoch=20, verbose=1, batch_size=100, callbacks=[history])
+res = model.fit(X_train, Y_train, nb_epoch=20, verbose=1, batch_size=100, callbacks=[dummy])
 
 
-print('done')
+t1=time.time()
+elapsed = t1-t0
+print('done {}'.format(elapsed))
+#done 4.033189535140991
