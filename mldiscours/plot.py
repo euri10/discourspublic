cccdbffd83d70ec2ac3e33471a6bf268f652690c
euri10 <benoit.barthelet@gmail.com>
2017-01-23 17:57:16 +0100

    force update at the end of epoch, inspired by the Progbar >_) lines are thiner added epoch line

--- mldiscours/plot.py
+++ mldiscours/plot.py
@@ -36,9 +36,13 @@ class Scope2(object):
         self.ax = ax
         self.x = [0]
         self.y = [0]
+        self.e_x = [0]
+        self.e_y = [0]
         self.markers = []
-        self.line = Line2D(self.x, self.y, marker='D', markeredgecolor='r', markevery=self.markers)
+        self.line = Line2D(self.x, self.y,linewidth=.1 , marker='D', markeredgecolor='r', markevery=self.markers)
+        self.line_epoch = Line2D(self.e_x, self.e_y,linewidth=.3 , color='r')
         self.ax.add_line(self.line)
+        self.ax.add_line(self.line_epoch)
         self.ax.set_ylim(-.1, 1.1)
         self.ax.set_xlim(0, len(self.x))
         plt.ion()
@@ -50,23 +54,26 @@ class Scope2(object):
     def _update_markers(self, markers):
         self.markers = markers
 
-    def update(self, x, y):
+    def _update(self, x, y, e_x, e_y, force=False):
         print(self.last_update)
         self.x = x
         self.y = y
+        self.e_x = e_x
+        self.e_y = e_y
         now = time.time()
         if self.verbose == 1:
-            if (now - self.last_update) < self.interval:
+            if not force and (now - self.last_update) < self.interval :
                 return
             else:
                 # print('NONONO')
                 self.ax.set_xlim(0, len(self.x))
                 self.line.set_data(self.x, self.y)
-                self.line.set_markevery(every = self.markers)
+                self.line_epoch.set_data(self.e_x, self.e_y)
+                # self.line.set_markevery(every = self.markers)
                 # following line is time consuming
                 self.ax.figure.canvas.draw()
                 self.last_update = now
-                return self.line,
+                return self.line, self.line_epoch
 
 
 class DummyCallback(Callback):
@@ -108,7 +115,7 @@ class DummyCallback(Callback):
         print('training done')
 
 class PlotLive(Callback):
-    """Callback that outputs a live plot.
+    """Callback that outputs a live matplotlib plot.
     """
     def __init__(self, scope):
         self.scope = scope
@@ -119,10 +126,14 @@ class PlotLive(Callback):
         self.nb_epoch = self.params['nb_epoch']
         self.nb_sample = self.params['nb_sample']
         self.batch_size = self.params['batch_size']
+        print('verbose{} nb_epoch{} nb_sample{} batch_size{}'.format(self.verbose, self.nb_epoch, self.nb_sample, self.batch_size))
         self.i = 0
+        self.e = 0
         self.x = np.zeros((int((self.nb_sample / self.batch_size)) + 1) * self.nb_epoch)
         self.y = np.zeros((int((self.nb_sample / self.batch_size)) + 1) * self.nb_epoch)
-        self.total_seen = 0
+        self.e_x = np.zeros(self.nb_epoch)
+        self.e_y = np.zeros(self.nb_epoch)
+
 
     def on_epoch_begin(self, epoch, logs={}):
         if self.verbose:
@@ -136,7 +147,6 @@ class PlotLive(Callback):
     def on_batch_end(self, batch, logs={}):
         batch_size = logs.get('size', 0)
         self.seen += batch_size
-        self.total_seen += batch_size
         # print('\n {}'.format(self.seen))
         self.x[self.i] = self.i
         self.y[self.i] = logs['loss']
@@ -148,18 +158,25 @@ class PlotLive(Callback):
         # skip progbar update for the last batch;
         # will be handled by on_epoch_end
         if self.verbose and self.seen < self.params['nb_sample']:
-            self.scope.update(self.x[:self.i], self.y[:self.i])
+            # self.scope._update(self.x[:self.i], self.y[:self.i])
+            self.scope._update(self.x[:self.i], self.y[:self.i],
+                               self.e_x[:self.i], self.e_y[:self.i])
 
     def on_epoch_end(self, epoch, logs={}):
         for k in self.params['metrics']:
             if k in logs:
                 self.log_values.append((k, logs[k]))
+        self.e_x[self.e] = self.i
+        self.e_y[self.e] = logs['loss']
+        self.e += 1
         if self.verbose:
-            # self.scope.update(self.seen)
             self.markers.append(self.i)
             self.scope._update_markers(self.markers)
-            self.scope.update(self.x[:self.i], self.y[:self.i])
-
+            if epoch < self.nb_epoch :
+                self.scope._update(self.x[:self.i], self.y[:self.i], self.e_x[:self.e], self.e_y[:self.e])
+            else:
+                self.scope._update(self.x[:self.i], self.y[:self.i],
+                                   self.e_x[:self.e], self.e_y[:self.e], force=True)
 
     def on_train_end(self, logs={}):
         print('training done')
@@ -187,12 +204,12 @@ def plotme():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     scope = Scope2(ax)
-    ani = FuncAnimation(fig, scope.update)
+    ani = FuncAnimation(fig, scope._update)
     # ion needed in scope
     plt.show()
     history = PlotLive(scope=scope)
     dummy = DummyCallback()
-    res = model.fit(X_train, Y_train, nb_epoch=20, verbose=1, batch_size=100, callbacks=[history])
+    res = model.fit(X_train, Y_train, nb_epoch=4, verbose=1, batch_size=100, callbacks=[history])
     # res = model.fit(X_train, Y_train, nb_epoch=5, verbose=1, batch_size=100, callbacks=[dummy])
 
 
